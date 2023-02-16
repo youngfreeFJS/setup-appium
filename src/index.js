ddb0432b492b5f4ed8b0f3ada7e25a712c00d550
youngfreeFJS <youngfreeFJS@gmail.com>
2023-02-16 15:35:09 +0800

    func(core): add error log

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const { execSync } = require('child_process')
-const { setFailed } = require('@actions/core')
+const { setFailed, info } = require('@actions/core')
 try {
   const installCommand = `npm install -g appium@next`
   const installReturn = execSync(installCommand, { shell: true, stdio: 'pipe' })
+  info(`
+      pid: ${installReturn.pid}
+      output: ${installReturn.output}
+      stdout: ${installReturn.stdout}
+      stderr: ${installReturn.stderr}
+      status: ${installReturn.status}
+      signal: ${installReturn.signal}
+      error: ${installReturn.error}
+    `)
   if (installReturn.status !== 0) {
-    setFailed(`install appium failed. original error: ${installReturn.stderr}`)
+    setFailed(`install appium failed. original error: ${installReturn.pid}`)
     return
   }
 } catch (error) {
